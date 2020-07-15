58d3b2b7674a592bda1c479803ddc400f2bc8e90
ai.kawakami <ai.kawakami@widetec.com>
2020-07-15 09:30:00 +0900

    tenki

--- bot.js
+++ bot.js
@@ -22,7 +22,9 @@ client.on('message', async message => {
         return;
    }
 
+    // 挨拶
 	if (message.content.match(/おは/)) {
+		//メッセージを送るチャンネルを判断します
         let channel = message.channel;
         let author = message.author.username;
         let reply_text =`おはゆ！！`;
@@ -41,12 +43,64 @@ client.on('message', async message => {
 		});
 		req.end();
         */
-        
+        //reply_textを送信します
         message.reply(reply_text)
+        	//メッセージを送信したら送信したメッセージをターミナルにも表示します
             .then(message => console.log(`Sent message: ${reply_text}`))
             .catch(console.error);
         return;
     }
+    
+    // 天気
+    if (message.content.match(/天気/)) {
+        let location = "Tokyo";
+        let APIKEY = "Your apikey";
+        //let URL = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + APIKEY;
+        let URL = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010";
+
+        http.get(URL, (res) => {
+            let body = "";
+            res.setEncoding("utf8");
+
+            res.on("data", (chunk) => {
+                body += chunk;
+            });
+
+            res.on("end", (res) => {
+                res = JSON.parse(body);
+                console.log(res);
+                let channel = message.channel;
+                let temp = res.main.temp - 273;
+                message.channel.send({
+                    embed: {
+                        color: 3447003,
+                        author: {
+                            name: 'OpenWeatherMap',
+                            icon_url: 'https://png.icons8.com/dusk/64/summer.png',
+                        },
+                        title: '長野のお天気情報',
+                        url: 'https://openweathermap.org',
+                        description: 'OpenWeatherMapのAPI叩いたデータです。',
+                        fields: [
+                            { name: '天気', value: res.weather[0].main },
+                            { name: '気温', value: temp.toFixed(2) + '°C' },
+                            { name: '風力', value: res.wind.speed + 'm' },
+                            { name: '雲量', value: res.clouds.all + '%' },
+                        ],
+                        timestamp: new Date(),
+                        footer: {
+                            icon_url: 'http://openweathermap.org/img/w/' + res.weather[0].icon.replace('n', 'd') + '.png',
+                            text: 'OverWatchから逃げるな！私ですか？逃げました',
+                        },
+                    }
+                })
+            });
+        }).on("error", (e) => {
+            console.log(e.message);
+        });
+    }    
+    
+    
 })
 
 // Discordへの接続
