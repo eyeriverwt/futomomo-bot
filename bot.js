e9ce29b844023736d6a6e8fa6942a37225a613aa
ai.kawakami <ai.kawakami@widetec.com>
2020-09-09 09:55:41 +0900

    add Zeller

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ client.on('message', async message => {
 		}
 		
 		//$day = ($year + $year / 4 - $year / 100 + $year / 400 + (13 * $month + 8)/ 5 + $date) % 7;
-		let day = ( year + (year/4) - (year/100) + (year/400) + ((13 * month + 8)/5) + date ) % 7;
+		let day = Math.floor( year + Math.floor(year/4) - Math.floor(year/100) + Math.floor(year/400) + Math.floor((13 * month + 8)/5) + date ) % 7;
 
 		//メンションせず
 		message.channel.send(`${author}ちゃん、${result[1]}/${result[2]}/${result[3]} Zeller:hatching_chick:`);
